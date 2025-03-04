50933e07d3876571d4527de0d13f096ab3a4cc25
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-04 12:36:00 +0530

    UC2 using service

--- src/main/java/com/app/greetingapp/controller/GreetingController.java
+++ src/main/java/com/app/greetingapp/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package com.app.greetingapp.controller;
 
+import com.app.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greetings")
 public class GreetingController {
 
+//    @GetMapping
+//    public String getGreeting() {
+//        return "Hello, World!";
+//    }
+
+    //UC2
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping
     public String getGreeting() {
-        return "Hello, World!";
+        return greetingService.getGreetingMessage();
     }
 }
