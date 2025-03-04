80efe9adac64d9621e537fdb72df40e84d158fc4
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-04 13:44:12 +0530

    UC3 name message

--- src/main/java/com/app/greetingapp/controller/GreetingController.java
+++ src/main/java/com/app/greetingapp/controller/GreetingController.java
@@ -4,6 +4,7 @@ import com.app.greetingapp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,11 +17,21 @@ public class GreetingController {
 //    }
 
     //UC2
+//    @Autowired
+//    private GreetingService greetingService;
+//
+//    @GetMapping
+//    public String getGreeting() {
+//        return greetingService.getGreetingMessage();
+//    }
+
+    //UC3
     @Autowired
     private GreetingService greetingService;
 
     @GetMapping
-    public String getGreeting() {
-        return greetingService.getGreetingMessage();
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
     }
 }
---
+++ src/main/java/com/app/greetingapp/model/GreetingModel.java
@@ -0,0 +1,40 @@
+package com.app.greetingapp.model;
+
+public class GreetingModel {
+    private String firstName;
+    private String lastName;
+
+    //UC3
+    public GreetingModel(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMessage() {
+        if (firstName != null && lastName != null) {
+            return "Hello, " + firstName + " " + lastName + "!";
+        } else if (firstName != null) {
+            return "Hello, " + firstName + "!";
+        } else if (lastName != null) {
+            return "Hello, " + lastName + "!";
+        } else {
+            return "Hello, World!";
+        }
+    }
+}
